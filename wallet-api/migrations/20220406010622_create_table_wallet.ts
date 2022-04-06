b186fa80a25152bfa6beba244e382bf479cb541b
FelipeGXavier <felipexavier20015@gmail.com>
2022-04-06 16:48:30 -0300

    fix: enforce null check in wallet amount

--- wallet-api/migrations/20220406010622_create_table_wallet.ts
+++ wallet-api/migrations/20220406010622_create_table_wallet.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTableIfNotExists("wallets", builder => {
     builder.increments("id").primary()
     builder.uuid("guid").notNullable().unique().defaultTo(randomUUID())
-    builder.integer("amount")
+    builder.integer("amount").defaultTo(0).notNullable()
     builder.integer("account_id").notNullable()
     builder.foreign("account_id").references("id").inTable("accounts")
     builder.timestamps(true, true)
