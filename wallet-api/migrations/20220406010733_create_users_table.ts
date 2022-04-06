bde59283d2f6493ac9e8a60312cea5d74c38a2fd
FelipeGXavier <felipexavier20015@gmail.com>
2022-04-06 16:19:49 -0300

    fix: users migration foreign key

--- wallet-api/migrations/20220406010733_create_users_table.ts
+++ wallet-api/migrations/20220406010733_create_users_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTableIfNotExists("users", builder => {
     builder.increments("id")
     builder.string("cpf", 11).notNullable().unique()
-    builder.bigInteger("account_id").notNullable()
+    builder.integer("account_id").notNullable()
     builder.foreign("account_id").references("id").inTable("accounts")
     builder.timestamps(true, true)
   })
