532f4199c4c2712b36ddef88220e53c5bba4f353
FelipeGXavier <felipexavier20015@gmail.com>
2022-04-06 16:30:03 -0300

    feat: add primary flag to wallets table

---
+++ wallet-api/migrations/20220406192340_alter_table_wallets_add_primary_field.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable("wallets", builder => {
+    builder.boolean("primary_wallet").defaultTo(false).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table("wallets", builder => {
+    builder.dropColumn("primary_wallet")
+  })
+}
