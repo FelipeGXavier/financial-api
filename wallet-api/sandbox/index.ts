6c79d9114bf9c69470b95b373ab2ec118e658d9b
FelipeGXavier <felipexavier20015@gmail.com>
2022-04-19 15:03:36 -0300

    feat: add Either custom type

--- wallet-api/sandbox/index.ts
+++ wallet-api/sandbox/index.ts
@@ -2,8 +2,8 @@ import { AccountRepositoryImpl } from "@/customer/infra/persistence/accountRepos
 import { connection } from "@/shared/defaultDatasource"
 import { LoadWalletService } from "@/transaction/application/usecases/loadWalletService"
 import { WalletRepositoryImpl } from "@/transaction/infra/persistence/walletRepository"
-import { request, response } from "express"
-import { WalletController } from "../src/transaction/infra/web/walletController"
+import { WalletController } from "@/transaction/infra/web/walletController"
+import { right, left, Either } from "@/shared/either"
 
 async function test() {
   const s = new LoadWalletService(
@@ -12,8 +12,23 @@ async function test() {
   )
 
   const c = new WalletController(s)
+}
 
-  console.log(c.loadWallet(request, response))
+class CustomErroName extends Error {}
+
+function testEither(n: number): Either<CustomErroName, Boolean> {
+  if (n <= 0) {
+    return left(new CustomErroName("error"))
+  }
+  return right(true)
+}
+
+const res = testEither(1)
+
+if (res.isLeft()) {
+  console.error(`Error ${res.value.message}`)
+} else {
+  console.log(`Success ${res.value}`)
 }
 
 test()
