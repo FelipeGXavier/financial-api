9412106320f7cb3b8b3fed5b33035e997908d1b4
FelipeGXavier <felipexavier20015@gmail.com>
2022-05-18 20:25:51 -0300

    test: add database helpers to use in jest hooks

---
+++ wallet-api/__tests__/fixture/databaseUtils.ts
@@ -0,0 +1,48 @@
+import { connection } from "@/shared/defaultDatasource"
+import path from "path"
+
+export const createSchemaAndMigrate = async () => {
+  await connection.raw(`
+    CREATE OR REPLACE
+    FUNCTION fresh() RETURNS void AS $$  
+
+    BEGIN
+    
+    DROP SCHEMA public CASCADE;
+
+    CREATE SCHEMA public;
+
+    GRANT ALL ON
+    SCHEMA public TO postgres;
+
+    GRANT ALL ON
+    SCHEMA public TO public;
+
+    END;
+
+    $$ LANGUAGE 'plpgsql';
+ `)
+  // Recreate schema
+  await connection.raw(`SELECT fresh();`)
+  // Migrate
+  await connection.migrate.latest({
+    directory: path.join(__dirname, "../../migrations"),
+  })
+}
+
+export const clearAllTables = async () => {
+  await connection.raw(`
+    CREATE OR REPLACE FUNCTION truncate_tables() RETURNS void AS $$
+    DECLARE
+    statements CURSOR FOR
+        SELECT tablename FROM pg_tables
+        WHERE schemaname = 'public';
+    BEGIN
+        FOR stmt IN statements LOOP
+            EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+        END LOOP;
+    END;
+    $$ LANGUAGE plpgsql;
+  `)
+  await connection.raw(`SELECT truncate_tables();`)
+}
--- wallet-api/__tests__/integration/transaction/transactionController.ts
+++ wallet-api/__tests__/integration/transaction/transactionController.ts
@@ -1,8 +1,20 @@
 import { app } from "__tests__/config/globalSetupServer"
+import {
+  createSchemaAndMigrate,
+  clearAllTables,
+} from "__tests__/fixture/databaseUtils"
 
 const server = app
 
 describe("TransactionController", () => {
+  beforeAll(async () => {
+    await createSchemaAndMigrate()
+  })
+
+  beforeEach(async () => {
+    await clearAllTables()
+  })
+
   test("GET /ping", async () => {
     const result = await server.get("/ping")
     expect(result.statusCode).toEqual(200)
