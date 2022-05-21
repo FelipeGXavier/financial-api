9a746219ec8ac92cce12851441dde38c1c20e474
FelipeGXavier <felipexavier20015@gmail.com>
2022-05-21 17:55:19 -0300

    test: add invalid request parameters integration test for transaction use case

--- wallet-api/__tests__/integration/transaction/transactionController.ts
+++ wallet-api/__tests__/integration/transaction/transactionController.ts
@@ -1,5 +1,6 @@
 import { app } from "__tests__/config/globalSetupServer"
 import { connection } from "@/shared/defaultDatasource"
+import { randomUUID } from "crypto"
 import {
   createSchemaAndMigrate,
   clearAllTables,
@@ -23,5 +24,33 @@ describe("TransactionController", () => {
   test("GET /ping", async () => {
     const result = await server.get("/ping")
     expect(result.statusCode).toEqual(200)
+  })
+
+  test("POST /api/transaction - Invalid parameters should return bad request", async () => {
+    const payeeGuid = randomUUID()
+    const payerGuid = randomUUID()
+    // Undefined value
+    const reqUndefinedValue = { payee: payeeGuid, payer: payerGuid }
+    // Undefined payee
+    const reqUndefinedPayee = { payer: payerGuid, value: 1 }
+    // Undefined payer
+    const reqUndefinedPayer = { payee: payeeGuid, value: 1 }
+    // Invalid uuid
+    const reqInvalidGuid = { payee: "", payer: "", value: 1 }
+    // Negative amount
+    const reqNegativeAmount = { payee: payeeGuid, value: -1 }
+    const badRequestMessage = "Invalid transaction parameters"
+    const valuesToBeTested = [
+      reqUndefinedValue,
+      reqUndefinedPayee,
+      reqUndefinedPayer,
+      reqInvalidGuid,
+      reqNegativeAmount,
+    ]
+    valuesToBeTested.forEach(async body => {
+      const result = await server.post("/api/transaction").send(body)
+      expect(result.statusCode).toEqual(400)
+      expect(result.body.message).toEqual(badRequestMessage)
+    })
   })
 })
